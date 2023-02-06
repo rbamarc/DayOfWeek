c7327cba777c482d2bdf01e2da623b962c1ac42d
rbamarc <rbamarc929@gmail.com>
2023-02-06 02:32:12 -0800

    changing to switch statement

--- src/main/java/netbeans/dayofweek/DayOfWeek.java
+++ src/main/java/netbeans/dayofweek/DayOfWeek.java
@@ -12,24 +12,40 @@ public class DayOfWeek {
         int day =4;
         String dayName = "";
         
-        //convert to if-else statement, once the correct condition is met
-        //the program will not continue 
-        if (day == 1) {
-            dayName = "Monday";
-        } else if (day == 2) {
-            dayName = "Tuesday";
-        } else if (day == 3) {
-            dayName = "Wednesday";
-        } else if (day == 4) {
-            dayName = "Thursday";
-        } else if (day == 5) {
-            dayName = "Friday";
-        } else if (day == 6) {
-            dayName = "Saturday";
-        } else if (day == 7) {
-            dayName = "Sunday";
-        } else {
-            dayName = "Oops!";
+        //convert to switch statement, once the correct condition is met
+        //the program will not continue
+        switch (day) {
+            case 1:
+                dayName = "Monday";
+                break;
+            case 2:
+                dayName = "Tuesday";
+                break;
+            case 3:
+                dayName = "Wednesday";
+                break;
+            case 4:
+                dayName = "Thursday";
+                break;
+            case 5:
+                dayName = "Friday";
+                break;
+            case 6:
+                dayName = "Saturday";
+                break;
+            case 7:
+                dayName = "Sunday";
+                break;
+            default:
+                dayName = "Invalid day";
+                break;
+                
+            /*
+                the big difference between an IF statement and a SWITCH statement
+                is that if statements can only evaluate as booleans. Switch 
+                Statements can evaluate any data type because its checking
+                to see if they match any of the other cases
+                */
         }
               
         System.out.println("The day is " + dayName);
